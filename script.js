271dad893183a70ffbb81e172edd5f106d840945
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-06-10 21:50:25 +0200

    copy files from codepen to git repository

---
+++ script.js
@@ -0,0 +1,25 @@
+const placeholder = document.getElementById('placeholder');
+const input = document.getElementById('input');
+
+const hideElement = () => {
+  input.classList.remove("hidden")
+  placeholder.classList.add("hidden")
+  input.focus()
+  input.value = placeholder.innerHTML
+}
+
+placeholder.addEventListener('click', () => hideElement());
+
+const changeText = () => {
+  input.classList.add("hidden")
+  placeholder.classList.remove("hidden")
+  placeholder.innerHTML = input.value
+}
+
+input.addEventListener('focusout', () => changeText());
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    changeText();
+  }
+});
